8433d57bd4d00de0186cfe0bbee2a5b0fedf3941
Albin Sjölin <albinsjoelin1@gmail.com>
2020-12-11 16:55:59 +0100

    feat: add search filters and extended search

--- api/user-position/controllers/user-position.js
+++ api/user-position/controllers/user-position.js
@@ -5,4 +5,76 @@
  * to customize this controller
  */
 
-module.exports = {};
+const geodist = require('geodist');
+
+function compareGeoPoint(a, b, userGeoPoint) {
+  const { lat, lng } = userGeoPoint;
+  const aDistanceFromPerson = geodist({ lat, lng, }, { lat: a.lat, lng: a.lng });
+  const bDistanceFromPerson = geodist({ lat, lng, }, { lat: b.lat, lng: b.lng });
+
+  return aDistanceFromPerson > bDistanceFromPerson;
+}
+
+function filterDistanceAndTime({
+  last_seen,
+  userGeoPoint,
+  objectGeoPoint,
+  maxDistance,
+  maxTime,
+}) {
+  const { lat: userLat, lng: userLng } = userGeoPoint;
+  const { lat: objectLat, lng: objectLng } = objectGeoPoint;
+  const distance = geodist({ lat: userLat, lng: userLng }, { lat: objectLat, lng: objectLng });
+
+  return (Date.now() - last_seen) <= maxTime && distance <= maxDistance;
+}
+
+module.exports = {
+  findWithPos(ctx) {
+    const { lat, lng, userId } = ctx.params;
+    const userGeoPoint = { lat, lng };
+
+    return strapi
+      .query('user-position')
+      .find()
+      .then(res => res
+        .filter(({ userId: objectId }) => userId !== objectId)
+        .filter(({ last_seen, lat: objectLat, lng: objectLng }) =>
+          filterDistanceAndTime({
+            last_seen,
+            objectGeoPoint: {
+              lat: objectLat,
+              lng: objectLng
+            },
+            userGeoPoint,
+            maxDistance: 50,
+            maxTime: 600_000,
+          }))
+        .sort((a, b) => compareGeoPoint(a, b, userGeoPoint))
+      );
+  },
+
+  findWithPosExtended(ctx) {
+    const { lat, lng, userId } = ctx.params;
+    const userGeoPoint = { lat, lng };
+
+    return strapi
+      .query('user-position')
+      .find()
+      .then(
+        res => res
+          .filter(({ userId: objectId }) => userId !== objectId)
+          .filter(({ last_seen, lat: objectLat, lng: objectLng }) =>
+            filterDistanceAndTime({
+              last_seen,
+              objectGeoPoint: {
+                lat: objectLat,
+                lng: objectLng
+              },
+              userGeoPoint,
+              maxTime: 600_000_000,
+            }))
+          .sort((a, b) => compareGeoPoint(a, b, userGeoPoint))
+      );
+  },
+};
--- api/user-position/services/user-position.js
+++ api/user-position/services/user-position.js
@@ -5,11 +5,4 @@
  * to customize this service
  */
 
-module.exports = {
-  find(params, populate) {
-    return strapi
-      .query('user-position')
-      .find(params, populate)
-      .then(res => res.filter(({ last_seen }) => (Date.now() - last_seen) <= 600_000));
-  },
-};
+module.exports = {};
