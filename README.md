cb0fd0318d9644b358d91eeebdcebe31dbdc27c0
Albin Sjölin <albinsjoelin1@gmail.com>
2020-12-29 16:39:31 +0100

    feat(user-position): add farfromuser string

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Start command
 kör från home, skriv cd för att komma till home
+kör sedan: 
 sudo docker run -it -d -p 1337:1337 -v `pwd`/nice-strapi-backend:/srv/app strapi/strapi
--- api/user-position/controllers/user-position.js
+++ api/user-position/controllers/user-position.js
@@ -15,6 +15,22 @@ function compareGeoPoint(a, b, userGeoPoint) {
   return aDistanceFromPerson > bDistanceFromPerson;
 }
 
+function getFarStringFromGeoPoint(usrLat, usrLng, objLat, objLng) {
+  const distance = geodist({ lat: usrLat, lng: usrLng }, { lat: objLat, lng: objLng });
+
+  switch (distance) {
+    case distance < 5000:
+      return 'Very far';
+
+    case distance < 2500:
+      return 'Far';
+
+    case distance < 1500:
+      return 'Nearby';
+  }
+  return 'Around the corner';
+}
+
 function filterDistance({
   userGeoPoint,
   objectGeoPoint,
@@ -41,7 +57,12 @@ module.exports = {
       .query('user-position')
       .find()
       .then(res => {
-        const allArrWithoutUser = res.filter(({ userId: objectId }) => userId !== objectId);
+        const allArrWithoutUser = res
+          .filter(({ userId: objectId }) => userId !== objectId)
+          .map((obj) => {
+
+            return { ...obj, farFromUser: getFarStringFromGeoPoint(lat, lng, obj.lat, obj.lng) };
+          });
         const timeFilteredArr = allArrWithoutUser.filter(({ last_seen }) => filterTime({ last_seen, maxTime: 600_000 }));
 
         return [
